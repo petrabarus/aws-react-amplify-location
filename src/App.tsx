52f4f74bc3829f8113b3d5310cde0cad390563c3
EC2 Default User <ec2-user@ip-172-31-33-47.ec2.internal>
2021-03-24 09:23:47 +0000

    feat: map.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from "react";
+import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
+import { Signer, ICredentials } from "@aws-amplify/core";
+import { Auth } from "aws-amplify";
+
+import ReactMapGL, {
+  NavigationControl,
+  ViewportProps,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import "./index.css";
+import amplifyConfig from "./aws-exports";
+
+Amplify.configure(amplifyConfig);
+
+const mapName = "MyMap";
+
+
+const transformRequest = (credentials: ICredentials) => (
+  url?: string,
+  resourceType?: string
+) => {
+  // Resolve to an AWS URL
+  if (resourceType === "Style" && !url?.includes("://")) {
+    url = `https://maps.geo.${amplifyConfig.aws_project_region}.amazonaws.com/maps/v0/maps/${url}/style-descriptor`;
+  }
+
+  // Only sign AWS requests (with the signature as part of the query string)
+  if (url?.includes("amazonaws.com")) {
+    return {
+      url: Signer.signUrl(url, {
+        access_key: credentials.accessKeyId,
+        secret_key: credentials.secretAccessKey,
+        session_token: credentials.sessionToken,
+      }),
+    };
+  }
+
+  // Don't sign
+  return { url: url || "" };
+};
+
+
+const App = () => {
+  const [credentials, setCredentials] = React.useState<ICredentials | null>(
+    null
+  );
+  
+  const [viewport, setViewport] = React.useState<Partial<ViewportProps>>({
+    longitude: 106.816666,
+    latitude: -6.200000,
+    zoom: 10,
+  });
+  
+  
+  React.useEffect(() => {
+    const fetchCredentials = async () => {
+      setCredentials(await Auth.currentUserCredentials());
+    };
+
+    fetchCredentials();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+    
+    {credentials ? (
+        <ReactMapGL
+          {...viewport}
+          width="100%"
+          height="100vh"
+          transformRequest={transformRequest(credentials)}
+          mapStyle={mapName}
+          onViewportChange={setViewport}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ position: "absolute", left: 20, top: 20 }}>
+            {/* react-map-gl v5 doesn't support dragging the compass to change bearing */}
+            <NavigationControl showCompass={false} />
+          </div>
+        </ReactMapGL>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    
     </div>
   );
 }
